c4fcf6aa1f01a07483581078a8c0ef2a89465eb0
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-02 17:48:05 +0200

    Create Service Extensions

---
+++ src/CovenantCouncil.Core/CoreServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TIKSN.DependencyInjection;
+
+namespace CovenantCouncil.Core;
+
+public static class CoreServiceExtensions
+{
+  public static IServiceCollection AddCoreServices(
+    this IServiceCollection services)
+  {
+    services.AddFrameworkCore();
+
+    return services;
+  }
+}
